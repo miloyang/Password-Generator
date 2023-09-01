91013fb5bd2579b591bcb5bd769c249e0f15d622
M Yang <miloyang9@gmail.com>
2023-09-01 17:42:45 +1000

    updated javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,7 @@
 // Assignment code here
+var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var specialCharacters = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\', ']', '^', '_', '`', '{', '|', '}', '~']
 
 
 // Get references to the #generate element
