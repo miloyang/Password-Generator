e42f8e3efa222c0496c36240624a956a44b924d7
M Yang <miloyang9@gmail.com>
2023-09-05 16:19:08 +1000

    added psuedo code and updated javascript code

---
+++ assets/javascript/PseudoCode.js
@@ -0,0 +1,23 @@
+//This program will allow the user to generate random password that meets their requirements.
+
+//Start Program 
+//Prompt to show (how many characters are required?) - User input required. Only allow numbers from 8-128.
+//Once user has entered a number between 8-128, continue 
+//If user has entered a number less than 8, or more than 128, show alert "Invalid number, please enter a number between 8 - 128 only."
+
+//Confirm to show (if numeric characters are required?) - user input required
+//If true, add the numeric characters variable and continue. If false, remove the numeric characters variable and continue. 
+
+//Confirm to show (if lowercase characters are required?) - user input required
+//If true, add the lowercase variable and continue. If false, remove the lowercase variable and continue. 
+
+//Confirm to show (if uppercase characters are required?) - user input required
+//If true, add the uppercase variable and continue. If false, remove the uppercase variable and continue. 
+
+//Confirm to show (if special characters are required?) - user input required
+//If true, add the special characters variable and continue. If false, remove the special characters variable and continue. 
+
+//If the user has choosen false for all 4 character requirements, alert to show "Invalid, please choose at least one character type."
+
+//Once all prompts have been entered, a random mix of characters that meet the required criteria to be displayed as an alert.
+
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,49 @@
 // Assignment code here
-var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var specialCharacters = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\', ']', '^', '_', '`', '{', '|', '}', '~']
 
+var passwordCharacters = {
+  "lowercaseCharacters": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  "uppercaseCharacters": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  "numericCharacters": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  "specialCharacters": ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+};
+
+function generatePassword() {
+  var characterLength = window.prompt("Enter the number of character required. (Min - 8, Max - 128)");
+  //Once user has entered a number between 8-128, continue 
+  //If user has entered a number less than 8, or more than 128, show alert ("Invalid number, please enter a number between 8 - 128 only.")
+
+  if (characterLength < 8 || characterLength > 128) {
+    return alert("Invalid number. Please enter a number between 8 - 128 only.")
+  }
+
+  var confirmLowercase = window.confirm("Do you require lowercase characters?");
+  var confirmUppercase = window.confirm("Do you require uppercase characters?");
+  var confirmNumeric = window.confirm("Do you require numeric characters?");
+  var confirmSpecial = window.confirm("Do you require special characters?");
+
+  if (confirmLowercase === false && confirmUppercase === false && confirmNumeric === false && confirmSpecial === false) {
+    return alert("Invalid input. Please select at least one type of character.")
+  }
+
+  var pass = [];
+
+  if (confirmLowercase === true) {
+    pass = pass.concat(passwordCharacters.lowercaseCharacters);
+  }
+
+  if (confirmUppercase === true) {
+    pass = pass.concat(passwordCharacters.uppercaseCharacters);
+  }
+
+  if (confirmNumeric === true) {
+    pass = pass.concat(passwordCharacters.numericCharacters);
+  }
+
+  if (confirmSpecial === true) {
+    pass = pass.concat(passwordCharacters.specialCharacters);
+  }
+//console.log(pass);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
