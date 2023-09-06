1f82ce037408a6872607dafbf8ac1933533805f5
M Yang <miloyang9@gmail.com>
2023-09-06 10:38:53 +1000

    updated javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -41,6 +41,17 @@ function generatePassword() {
   if (confirmSpecial === true) {
     pass = pass.concat(passwordCharacters.specialCharacters);
   }
+
+  for (var i = 0; i <= characterLength; i++) {
+    pass += pass.charAt(Math.floor(Math.random() * passwordCharacters.Length));
+  }
+
+  // for (var i = 0; i <= characterLength; i++) {
+  //   function passRandom(pass) {
+  //     return Math.floor(Math.random() * pass);
+  //   }
+  // }
+  return pass;
 }
 
 // Get references to the #generate element
@@ -57,3 +68,31 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Initiate a for loop to iterate through the number of password characters declared in characterLength prompt
+
+  // Inside the for loop consider using Math.random() to randomize an array index
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+  // Access the array index using bracket notation Example: lowercaseCharacters[4] 
+  // In this example 4 could be replaced by a variable that was randomly generated
+  // You could create an empty array and use the push method (An array method) to add each character
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+  // Then use another array method (join) to stringify them
+  // Try searching join in the mozilla (MDN) documentation
+//If you had say newPassword = [] for the array. You could use something like  return newPassword.join()
+
+
+
+
+
+
+
+
+
+
+// for (var i = 0; i <= passwordLength; i++) {
+//   var randomNumber = Math.floor(Math.random() * chars.length);
+//   password += chars.substring(randomNumber, randomNumber +1);
+// }
